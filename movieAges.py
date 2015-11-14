a5185b07953642afa595111e3004f9d7c4e2fdc9
roguishmountain <roguishmountain@gmail.com>
2015-11-14 13:38:27 -0500

    downloads movie page for each movie
    used to get cast list, having issues retrieving nested tags

--- movieAges.py
+++ movieAges.py
@@ -1,6 +1,14 @@
 from HTMLParser import HTMLParser
+import os
 
 # add file download and name
+listOfMovies = []
+
+class Movie():
+    def __init__(self, title):
+        self.title = title
+        self.url = ""
+        self.actorName = []
 
 class MyHTMLParser(HTMLParser):
     def __init__(self):
@@ -13,33 +21,42 @@ class MyHTMLParser(HTMLParser):
 
         if tag == "img":
             for attr in attrs:
-                print attr
+                if attr[0].lower() == "class" and attr[1].lower() == "poster shadowed":
+                    name = True
+                if attr[0].lower() == "title" and name:
+                    #print "Title:", attr[1]
+                    listOfMovies.append(Movie(attr[1]))
 
         if tag == "a":
             for attr in attrs:
                 if attr[0].lower() == "href" and attr[1].lower() == "poster shadowed":
                     print "attr:", attr
+                elif attr[0].lower() == "href" and attr[1].startswith("/title") and attr[1].endswith("tt"):
+                    #print "link:", attr[1]
+                    listOfMovies[len(listOfMovies)-1].url = attr[1]
 
         if tag == "a":
             for name, value in attrs:
                 if name == "itemprop" and value == "url":
                     self.d = True
-                    print name, value
+                    #print name, value
 
-    def handle_data(self, data):
-        if self.d:
-            data = data.rstrip().lstrip()
-            if len(data) == 0:
-                self.end = False
-                self.d = True
+os.system("wget -q -O movieList http://www.imdb.com/movies-in-theaters/?ref_=inth_inth")
 
-            elif not self.end:
-                print data
-                self.d = False
-
-
-
-file = open("listofmovies", "r")
+file = open("movieList", "r")
 stream = file.read()
 parse = MyHTMLParser()
 parse.feed(stream)
+file.close()
+
+url_base = "http://www.imdb.com"
+for item in listOfMovies:
+    print item.title, url_base + item.url
+    actors = url_base + item.url
+    wget = "wget -q -O moviePage " + actors
+    os.system(wget)
+
+#<span class="itemprop" itemprop="name">Tenoch Huerta</span>
+for line in open("moviePage", "r"):
+    if '<span class="itemprop" itemprop="name">' in line:
+        print line
