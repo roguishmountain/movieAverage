1d698e69e185a7a6838a8bdb9785ec4f5e0f5c86
roguishmountain <roguishmountain@gmail.com>
2015-11-13 23:22:53 -0500

    decided to use a different html doc

--- movieAges.py
+++ movieAges.py
@@ -1,18 +1,45 @@
 from HTMLParser import HTMLParser
 
+# add file download and name
+
 class MyHTMLParser(HTMLParser):
-	
+    def __init__(self):
+        HTMLParser.__init__(self)
+        self.d = False
+        self.end = False
+
     def handle_starttag(self, tag, attrs):
-    	name = False
+        name = False
+
         if tag == "img":
-        	for attr in attrs:
-        		if attr[0].lower() == "class" and attr[1].lower() == "poster shadowed":
-        			name = True
-        		if attr[0].lower() == "title" and name:
-        			print "attr:", attr[1]
+            for attr in attrs:
+                print attr
+
+        if tag == "a":
+            for attr in attrs:
+                if attr[0].lower() == "href" and attr[1].lower() == "poster shadowed":
+                    print "attr:", attr
+
+        if tag == "a":
+            for name, value in attrs:
+                if name == "itemprop" and value == "url":
+                    self.d = True
+                    print name, value
+
+    def handle_data(self, data):
+        if self.d:
+            data = data.rstrip().lstrip()
+            if len(data) == 0:
+                self.end = False
+                self.d = True
+
+            elif not self.end:
+                print data
+                self.d = False
 
 
-file=open("movieList","r")
-stream=file.read()
+
+file = open("listofmovies", "r")
+stream = file.read()
 parse = MyHTMLParser()
 parse.feed(stream)
